50154edd8b1ec94dddab8c0b0a30f348caacb4d5
Ahmad <103906421+ahmadk953@users.noreply.github.com>
2022-11-02 16:49:20 -0400

    Add/Update Files

--- resource.h
+++ resource.h
@@ -4,6 +4,7 @@
 //
 #define IDC_MYICON                      2
 #define IDD_WINDOWSAPP1_DIALOG          102
+#define IDD_OPEN_MENU                   102
 #define IDS_APP_TITLE                   103
 #define IDD_ABOUTBOX                    103
 #define IDM_ABOUT                       104
@@ -12,6 +13,7 @@
 #define IDI_SMALL                       108
 #define IDC_WINDOWSAPP1                 109
 #define IDR_MAINFRAME                   128
+#define IDD_DIALOG1                     129
 #define ID_FILE_OPEN                    32771
 #define ID_FILE_SAVE                    32772
 #define ID_FILE_SAVEAS                  32773
@@ -27,7 +29,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32779
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           110
